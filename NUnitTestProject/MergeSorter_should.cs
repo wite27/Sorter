70bc1a2fe484f001c77a2672a9e815b0c4b0ab0a
wite27 <xquzyx@yandex.ru>
2017-03-27 23:22:52 +0300

    todo: fix merge sort

---
+++ NUnitTestProject/MergeSorter_should.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnitTestProject;
+using Sorter;
+using Sorter.SorterImplementations;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class MergeSorter_should : Sorter_should<int>
+    {
+        public override Sorter<int> GetSorter()
+        {
+            return new MergeSorter<int>();
+        }
+    }
+}
--- Sorter/SorterImplementations/MergeSorter.cs
+++ Sorter/SorterImplementations/MergeSorter.cs
@@ -6,11 +6,52 @@ using System.Threading.Tasks;
 
 namespace Sorter.SorterImplementations
 {
-    class MergeSorter<T> : Sorter<T> where T : IComparable<T>
+    public class MergeSorter<T> : Sorter<T> where T : IComparable<T>
     {
         public override void Sort(T[] coll)
         {
-            throw new NotImplementedException();
+            if (coll.Length == 0 || coll.Length == 1)
+                return;
+            sort(0, coll.Length-1, coll);
+        }
+
+        private void sort(int low, int high, T[] coll)
+        {
+            if (low == high)
+                return;
+
+            var med = (high + low) / 2;
+
+            sort(low, med, coll);
+            sort(med+1, high, coll);
+            merge(low, med, med+1, high, coll);
+        }
+
+        private void merge(int firstLow, int firstHigh, int secondLow, int secondHigh, T[] coll)
+        {
+            var firstCurrent  = firstLow;
+            var secondCurrent = secondLow;
+
+            var temp = new T[(firstHigh - firstLow + 1) + (secondHigh - secondLow + 1)];
+            var tempCurrent = 0;
+            while (firstCurrent <= firstHigh &&
+                   secondCurrent <= secondHigh)
+            {
+                if (coll[firstCurrent].CompareTo(coll[secondCurrent]) < 0)
+                {
+                    temp[tempCurrent++] = coll[firstCurrent++];
+                }
+                else
+                {
+                    temp[tempCurrent++] = coll[secondCurrent++];
+                }
+            }
+            while (firstCurrent <= firstHigh)
+                temp[tempCurrent++] = coll[firstCurrent++];
+            while (secondCurrent <= secondHigh)
+                temp[tempCurrent++] = coll[secondCurrent++];
+
+            Array.Copy(temp, coll, temp.Length);
         }
     }
 }
